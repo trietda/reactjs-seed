33a0f9fcd6dc1d93af2254d383537e5873c5e725
Triet Dang <datriet@gmail.com>
2023-03-07 15:54:50 +0700

    feat: antd

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import 'antd/dist/reset.css';
 import reportWebVitals from './reportWebVitals';
 import router from './router';
 
--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -1,6 +1,62 @@
+import { useState } from 'react';
+import {
+  Breadcrumb, Layout, Menu, theme,
+} from 'antd';
+import type { MenuProps } from 'antd';
+import { PieChartOutlined } from '@ant-design/icons';
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+const {
+  Header, Content, Footer, Sider,
+} = Layout;
+
+const items: MenuItem[] = [
+  {
+    key: '1',
+    label: 'Test',
+    icon: <PieChartOutlined />,
+  },
+  {
+    key: '2',
+    label: 'Test 2',
+    icon: <PieChartOutlined />,
+    children: [
+      {
+        key: '3',
+        label: 'Test 3',
+        icon: <PieChartOutlined />,
+      },
+    ],
+  },
+];
+
 function RootPage() {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
   return (
-    <div>Root</div>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+        <div style={{ height: 32, margin: 16, background: 'rgba(255, 255, 255, 0.2)' }} />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+      </Sider>
+      <Layout className="site-layout">
+        <Header style={{ padding: 0, background: colorBgContainer }} />
+        <Content style={{ margin: '0 16px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div style={{ padding: 24, minHeight: 360, background: colorBgContainer }}>
+            Bill is a cat.
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
+      </Layout>
+    </Layout>
   );
 }
 
